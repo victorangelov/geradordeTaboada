349e6adbcb989fb27a531d2222e8e5c31d8ee8dc
Victor Ângelo Sánchez da Silva <53021597+victorangelov@users.noreply.github.com>
2020-09-28 16:09:42 -0300

    exercicio taboada finalizado.
    exercicio taboada finalizado.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-//alert('Olá! mather fucker')
+function gerar(){
+    let num = window.document.querySelector("input#txtnumero")
+    let res = window.document.querySelector("select#celtab")
+
+    if(num.value.length == 0){
+       window.alert(`Por favor digite um número`) 
+       res.innerHTML = ``
+       let iten = document.createElement('option')
+       iten.text = `Digite um número acima`
+       res.appendChild(iten)
+    }else{
+        res.innerHTML = ``
+        for(var i = 1; i < 11; i++){
+            let iten = document.createElement('option')
+            iten.text = `${num.value} x ${i} = ${num.value * i}`
+            iten.value = `res${i}`
+            res.appendChild(iten)
+        }
+    }
+}
